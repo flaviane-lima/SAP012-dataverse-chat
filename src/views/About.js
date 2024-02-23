f2b8e58ccd99a4fafa74e012848dab96c97ee6e0
flaviane-lima <rflaviane41@gmail.com>
2024-02-23 10:59:16 -0300

    foram algumas alterações de adição de pasta

---
+++ src/views/About.js
@@ -0,0 +1,7 @@
+export function About(props) {
+  const viewEl = document.createElement('div');
+  viewEl.textContent = 'This is the About page.';
+  return viewEl;
+}
+
+// Definir funciones/componentes similares para otras rutas
---
+++ src/views/Home.js
@@ -0,0 +1,10 @@
+// src/views/Home.js
+
+export function Home(props) {
+  const viewEl = document.createElement('div');
+  viewEl.textContent = 'Welcome to the home page!';
+  return viewEl;
+}
+
+// src/views/About.js
+
