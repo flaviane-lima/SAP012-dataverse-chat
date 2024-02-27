93fe892bd0a0a46c416b6d9872dd33e93d5fd544
flaviane-lima <rflaviane41@gmail.com>
2024-02-26 22:23:20 -0300

    Foi feito a função do roteador de quatros funções

--- src/router.js
+++ src/router.js
@@ -2,15 +2,26 @@
 let ROUTES = {};
 let rootEl;
 
+const ERRORPATH = '/Error'
+
 export const setRootEl = (el) => {
   // atribui rootEl
+  console.log("setRootEl")
+  rootEl = el;
 
 }
 
 export const setRoutes = (routes) => {
   // optional Throw errors if routes isn't an object
+  if (typeof routes !== 'object') {
+    throw new Error('As rotas devem ser um objeto');
+  }  
   // optional Throw errors if routes doesn't define an /error route
+  if (!routes[ERRORPATH]) {
+    throw new Error('As rotas devem definir uma rota /error');
+  }  
   // assign ROUTES
+  ROUTES = routes;
 }
 
 // export const queryStringToObject = (queryString) => {
@@ -18,12 +29,27 @@ export const setRoutes = (routes) => {
 //   // convert URLSearchParams to an object
 //   // return the object
 // }
- const renderView = (pathname, props={}) => {
-  // clear the root element
-  // find the correct view in ROUTES for the pathname
-  // in case not found render the error view
-  // render the correct view passing the value of props
-  // add the view element to the DOM root element
+ const renderView = (pathname, props = {name: " ", id: ""}) => {
+  console.log("renderView");
+  //limpa o elemento raiz
+  rootEl.innerHTML = "";
+
+  let path = "";
+
+  //encontra a visualização correta em ROUTES para o nome do caminho
+  if(!pathname in ROUTES){
+    path = pathname;
+  }
+  else{
+    // caso não seja encontrado renderiza a view do erro
+    path =  ERRORPATH
+  }
+
+  // renderiza a view correta passando o valor das props
+ //adiciona o elemento view ao elemento raiz do DOM
+  rootEl.appendChild(ROUTES[path](props));
+
+  
 } 
 
 // export const navigateTo = (pathname, props={}) => {
@@ -32,7 +58,12 @@ export const setRoutes = (routes) => {
 // }
 
 export const onURLChange = (location) => {
-  // parse the location for the pathname and search params
-  // convert the search params to an object
-  // render the view with the pathname and object
+  console.log("onURLChange");
+  // analisa a localização do nome do caminho e dos parâmetros de pesquisa
+  
+
+  // converte os parâmetros de pesquisa em um objeto
+
+ // renderiza a view com o caminho e o objeto
+ renderView(location.pathname);
 }
