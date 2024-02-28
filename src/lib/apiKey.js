1062af4610c11a0b7f8682154122f0072ded9aee
flaviane-lima <rflaviane41@gmail.com>
2024-02-28 18:03:43 -0300

    criação do arquivo para a chave API

---
+++ src/lib/apiKey.js
@@ -0,0 +1,10 @@
+// src/lib/apiKey.js
+
+export const getApiKey = () => {
+  // Implemente o código para obter a API KEY do armazenamento local
+};
+
+export const setApiKey = (key) => {
+ // Implemente o código para salvar a API KEY no armazenamento local
+};
+
