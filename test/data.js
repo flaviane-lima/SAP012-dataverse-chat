7f3faa88a81fdb60d4809676738051cb95bb65e9
flaviane-lima <rflaviane41@gmail.com>
2024-02-28 18:05:52 -0300

    emigração dos dados para o teste da função do datafunction

---
+++ test/data.js
@@ -0,0 +1,310 @@
+export const data = [
+  {
+    "id": "cardcaptor-sakura",
+    "name": "Cardcaptor Sakura",
+    "shortDescription": "Comédia, Aventura",
+    "description": "Baseado no mangá de mesmo nome do grupo Clamp. A história gira em torno de Sakura Kinomoto, uma estudante do quarto ano que descobre poderes mágicos após libertar acidentalmente um conjunto de cartas mágicas chamadas Cartas Clow. Ela deve recuperar as cartas para evitar uma catástrofe no mundo. Cada carta tem poderes diferentes e só pode ser ativada por alguém com habilidades mágicas inerentes. O anime tem 70 episódios e dois filmes. Uma sequência chamada Cardcaptor Sakura: Clear Card começou a ser publicada em 2016.",
+    "imageUrl": "https://assets.gamearena.gg/wp-content/uploads/2023/12/04084244/cardcaptor-sakura.jpg",
+    "facts": {
+      "creatorOfManga": "CLAMP",
+      "producerOfManga": "Eizo Kondo",
+      "studioOfManga": "Madhouse"
+    },
+    "extraInfo": {
+      "seasons": 1,
+      "episodes": 22,
+      "classification": "L",
+      "assessment": 4.8,
+      "streaming": "Crunchyroll",
+      "song": ["Catch You Catch Me (interpretada por Gumi)"]
+    }
+  },
+  {
+    "id": "death-note",
+    "name": "Death Note",
+    "shortDescription": "Suspense, Sobrenatural, Psicológico",
+    "description": "Baseado no mangá de Tsugumi Ohba e Takeshi Obata. A história acompanha Light Yagami, um estudante genial que encontra um caderno sobrenatural: o “Death Note”, que pertencia ao shinigami Ryuk, e concede ao usuário a habilidade de matar qualquer pessoa cujo nome seja escrito em suas páginas. Light decide usar o caderno para criar um novo mundo livre de mal, mas entra em conflito com L, um detetive misterioso que tenta capturar e deter Kira, o apelido dado ao assassino em série.",
+    "imageUrl": "https://s2-techtudo.glbimg.com/1Q76caEdN3BriFZ_l61VD5MJkYs=/0x0:1280x720/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/g/0/s4BJiLQGSb9USN3vTvAg/death-note-netflix.jpg",
+    "facts": {
+      "creatorOfManga": "Tsugumi Ohba",
+      "producerOfManga": "Tetsuro Araqui",
+      "studioOfManga": "Madhouse"
+    },
+    "extraInfo": {
+      "seasons": 1,
+      "episodes": 37,
+      "classification": "A16",
+      "assessment": 4.8,
+      "streaming": "Netflix",
+      "song": ["the WORLD(interpretada banda japonesa Nightmare)"]
+    }
+  },
+  {
+    "id": "yu-yu-hakusho",
+    "name": "Yu Yu Hakusho",
+    "shortDescription": "Ação, Aventura, Comédia Dramática, Sobrenatural",
+    "description": "Baseado no mangá de Yoshihiro Togashi, que conta a história de Yusuke Urameshi, um delinquente juvenil que morre ao tentar salvar uma criança de um acidente de carro. Ele é ressuscitado e recebe o título de “Detetive Espiritual”, tendo que investigar casos envolvendo demônios e aparições no Mundo Humano. O anime se torna mais focado em lutas e torneios de artes marciais conforme avança. O anime tem 112 episódios, dois filmes, uma série de OVAs e uma série live-action. Yu Yu Hakusho é considerado um dos melhores animes dos anos 90.",
+    "imageUrl": "https://furansujapon.com/wp-content/uploads/2020/12/Yu-Yu-Hakusho.jpg",
+    "facts": {
+      "creatorOfManga": "Yoshihiro Togashi",
+      "producerOfManga": "Yoshihiro Togashi",
+      "studioOfManga": "Pierrot"
+    },
+    "extraInfo": {
+      "seasons": 1,
+      "episodes": 112,
+      "classification": "A16",
+      "assessment": 4.8,
+      "streaming": "Crunchyroll",
+      "song": ["Hohoemi no Bakudan(interpretado grupo Mawatari Matsuko)"]
+    }
+  },
+  {
+    "id": "bleach",
+    "name": "Bleach",
+    "shortDescription": "Ação, Aventura, Fantasia, Sobrenatural",
+    "description": "Bleach é um anime baseado no mangá de mesmo nome de Tite Kubo. A história segue as aventuras de Ichigo Kurosaki, um estudante que pode ver fantasmas e que ganha os poderes de um Shinigami (Ceifador de Almas) de Rukia Kuchiki. Com seus novos poderes, ele tem que defender os humanos dos espíritos malignos chamados Hollows e guiar as almas dos mortos para a Sociedade das Almas. O anime adapta o mangá e também inclui arcos originais com personagens e histórias exclusivas do anime.",
+    "imageUrl": "https://roteironerd.com/wp-content/uploads/2021/08/bleach.jpg",
+    "facts": {
+      "creatorOfManga": "Tite Kubo",
+      "producerOfManga": "Aniplex, Genco, DAX Production e mais",
+      "studioOfManga": "Pierrot"
+    },
+    "extraInfo": {
+      "seasons": 16,
+      "episodes": 366,
+      "classification": "A12",
+      "assessment": 4.7,
+      "streaming": "Star+",
+      "song": ["O anime tem várias músicas, cantadas por diferentes artistas como LiSA, Eir Aoi, Luna Haruna, Shirō Sagisu, entre outros"]
+    }
+  },
+  {
+    "id": "demon-slayer",
+    "name": "Demon Slayer",
+    "shortDescription": "Ação, Fantasia Sombria, Artes Marciais",
+    "description": "Baseado em um mangá, Demon Slayer é anime de fantasia sombria que conta a história de Tanjiro Kamado, um garoto que se torna um caçador de demônios depois que sua família é morta e sua irmã Nezuko é transformada em uma demônia. Tanjiro busca uma forma de curar Nezuko e vingar sua família, enfrentando vários demônios e aprendendo técnicas de respiração especiais. A obra se passa no Japão da era Taishō e explora temas como perseverança, amizade, bondade e sacrifício.",
+    "imageUrl": "https://s2-techtudo.glbimg.com/fscjBXjyh15CsR0umO4h0i8O05M=/0x0:1280x720/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/J/8/EbfA0BTv2pV98gASWdfA/f5d0f04e85a224493fe9caa2e59740811630901421-main.png",
+    "facts": {
+      "creatorOfManga": "Koyoharu Gotōge",
+      "producerOfManga": "Yuma Takahash",
+      "studioOfManga": "Shueisha"
+    },
+    "extraInfo": {
+      "seasons": 5,
+      "episodes": 56,
+      "classification": "A14",
+      "assessment": 4.9,
+      "streaming": "Netflix",
+      "song": ["Gurenge(interpretada LiSA)"]
+    }
+  },
+];
+
+export const mockCrescente = [
+  {
+    "id": "bleach",
+    "name": "Bleach",
+    "shortDescription": "Ação, Aventura, Fantasia, Sobrenatural",
+    "description": "Bleach é um anime baseado no mangá de mesmo nome de Tite Kubo. A história segue as aventuras de Ichigo Kurosaki, um estudante que pode ver fantasmas e que ganha os poderes de um Shinigami (Ceifador de Almas) de Rukia Kuchiki. Com seus novos poderes, ele tem que defender os humanos dos espíritos malignos chamados Hollows e guiar as almas dos mortos para a Sociedade das Almas. O anime adapta o mangá e também inclui arcos originais com personagens e histórias exclusivas do anime.",
+    "imageUrl": "https://roteironerd.com/wp-content/uploads/2021/08/bleach.jpg",
+    "facts": {
+      "creatorOfManga": "Tite Kubo",
+      "producerOfManga": "Aniplex, Genco, DAX Production e mais",
+      "studioOfManga": "Pierrot"
+    },
+    "extraInfo": {
+      "seasons": 16,
+      "episodes": 366,
+      "classification": "A12",
+      "assessment": 4.7,
+      "streaming": "Star+",
+      "song": ["O anime tem várias músicas, cantadas por diferentes artistas como LiSA, Eir Aoi, Luna Haruna, Shirō Sagisu, entre outros"]
+    }
+  },
+  {
+    "id": "cardcaptor-sakura",
+    "name": "Cardcaptor Sakura",
+    "shortDescription": "Comédia, Aventura",
+    "description": "Baseado no mangá de mesmo nome do grupo Clamp. A história gira em torno de Sakura Kinomoto, uma estudante do quarto ano que descobre poderes mágicos após libertar acidentalmente um conjunto de cartas mágicas chamadas Cartas Clow. Ela deve recuperar as cartas para evitar uma catástrofe no mundo. Cada carta tem poderes diferentes e só pode ser ativada por alguém com habilidades mágicas inerentes. O anime tem 70 episódios e dois filmes. Uma sequência chamada Cardcaptor Sakura: Clear Card começou a ser publicada em 2016.",
+    "imageUrl": "https://assets.gamearena.gg/wp-content/uploads/2023/12/04084244/cardcaptor-sakura.jpg",
+    "facts": {
+      "creatorOfManga": "CLAMP",
+      "producerOfManga": "Eizo Kondo",
+      "studioOfManga": "Madhouse"
+    },
+    "extraInfo": {
+      "seasons": 1,
+      "episodes": 22,
+      "classification": "L",
+      "assessment": 4.8,
+      "streaming": "Crunchyroll",
+      "song": ["Catch You Catch Me (interpretada por Gumi)"]
+    }
+  },
+  {
+    "id": "death-note",
+    "name": "Death Note",
+    "shortDescription": "Suspense, Sobrenatural, Psicológico",
+    "description": "Baseado no mangá de Tsugumi Ohba e Takeshi Obata. A história acompanha Light Yagami, um estudante genial que encontra um caderno sobrenatural: o “Death Note”, que pertencia ao shinigami Ryuk, e concede ao usuário a habilidade de matar qualquer pessoa cujo nome seja escrito em suas páginas. Light decide usar o caderno para criar um novo mundo livre de mal, mas entra em conflito com L, um detetive misterioso que tenta capturar e deter Kira, o apelido dado ao assassino em série.",
+    "imageUrl": "https://s2-techtudo.glbimg.com/1Q76caEdN3BriFZ_l61VD5MJkYs=/0x0:1280x720/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/g/0/s4BJiLQGSb9USN3vTvAg/death-note-netflix.jpg",
+    "facts": {
+      "creatorOfManga": "Tsugumi Ohba",
+      "producerOfManga": "Tetsuro Araqui",
+      "studioOfManga": "Madhouse"
+    },
+    "extraInfo": {
+      "seasons": 1,
+      "episodes": 37,
+      "classification": "A16",
+      "assessment": 4.8,
+      "streaming": "Netflix",
+      "song": ["the WORLD(interpretada banda japonesa Nightmare)"]
+    }
+  },
+  {
+    "id": "demon-slayer",
+    "name": "Demon Slayer",
+    "shortDescription": "Ação, Fantasia Sombria, Artes Marciais",
+    "description": "Baseado em um mangá, Demon Slayer é anime de fantasia sombria que conta a história de Tanjiro Kamado, um garoto que se torna um caçador de demônios depois que sua família é morta e sua irmã Nezuko é transformada em uma demônia. Tanjiro busca uma forma de curar Nezuko e vingar sua família, enfrentando vários demônios e aprendendo técnicas de respiração especiais. A obra se passa no Japão da era Taishō e explora temas como perseverança, amizade, bondade e sacrifício.",
+    "imageUrl": "https://s2-techtudo.glbimg.com/fscjBXjyh15CsR0umO4h0i8O05M=/0x0:1280x720/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/J/8/EbfA0BTv2pV98gASWdfA/f5d0f04e85a224493fe9caa2e59740811630901421-main.png",
+    "facts": {
+      "creatorOfManga": "Koyoharu Gotōge",
+      "producerOfManga": "Yuma Takahash",
+      "studioOfManga": "Shueisha"
+    },
+    "extraInfo": {
+      "seasons": 5,
+      "episodes": 56,
+      "classification": "A14",
+      "assessment": 4.9,
+      "streaming": "Netflix",
+      "song": ["Gurenge(interpretada LiSA)"]
+    }
+  },
+
+  {
+    "id": "yu-yu-hakusho",
+    "name": "Yu Yu Hakusho",
+    "shortDescription": "Ação, Aventura, Comédia Dramática, Sobrenatural",
+    "description": "Baseado no mangá de Yoshihiro Togashi, que conta a história de Yusuke Urameshi, um delinquente juvenil que morre ao tentar salvar uma criança de um acidente de carro. Ele é ressuscitado e recebe o título de “Detetive Espiritual”, tendo que investigar casos envolvendo demônios e aparições no Mundo Humano. O anime se torna mais focado em lutas e torneios de artes marciais conforme avança. O anime tem 112 episódios, dois filmes, uma série de OVAs e uma série live-action. Yu Yu Hakusho é considerado um dos melhores animes dos anos 90.",
+    "imageUrl": "https://furansujapon.com/wp-content/uploads/2020/12/Yu-Yu-Hakusho.jpg",
+    "facts": {
+      "creatorOfManga": "Yoshihiro Togashi",
+      "producerOfManga": "Yoshihiro Togashi",
+      "studioOfManga": "Pierrot"
+    },
+    "extraInfo": {
+      "seasons": 1,
+      "episodes": 112,
+      "classification": "A16",
+      "assessment": 4.8,
+      "streaming": "Crunchyroll",
+      "song": ["Hohoemi no Bakudan(interpretado grupo Mawatari Matsuko)"]
+    }
+  },
+];
+
+export const mockDecrescente = [
+  {
+    "id": "yu-yu-hakusho",
+    "name": "Yu Yu Hakusho",
+    "shortDescription": "Ação, Aventura, Comédia Dramática, Sobrenatural",
+    "description": "Baseado no mangá de Yoshihiro Togashi, que conta a história de Yusuke Urameshi, um delinquente juvenil que morre ao tentar salvar uma criança de um acidente de carro. Ele é ressuscitado e recebe o título de “Detetive Espiritual”, tendo que investigar casos envolvendo demônios e aparições no Mundo Humano. O anime se torna mais focado em lutas e torneios de artes marciais conforme avança. O anime tem 112 episódios, dois filmes, uma série de OVAs e uma série live-action. Yu Yu Hakusho é considerado um dos melhores animes dos anos 90.",
+    "imageUrl": "https://furansujapon.com/wp-content/uploads/2020/12/Yu-Yu-Hakusho.jpg",
+    "facts": {
+      "creatorOfManga": "Yoshihiro Togashi",
+      "producerOfManga": "Yoshihiro Togashi",
+      "studioOfManga": "Pierrot"
+    },
+    "extraInfo": {
+      "seasons": 1,
+      "episodes": 112,
+      "classification": "A16",
+      "assessment": 4.8,
+      "streaming": "Crunchyroll",
+      "song": ["Hohoemi no Bakudan(interpretado grupo Mawatari Matsuko)"]
+    }
+  },
+  {
+    "id": "demon-slayer",
+    "name": "Demon Slayer",
+    "shortDescription": "Ação, Fantasia Sombria, Artes Marciais",
+    "description": "Baseado em um mangá, Demon Slayer é anime de fantasia sombria que conta a história de Tanjiro Kamado, um garoto que se torna um caçador de demônios depois que sua família é morta e sua irmã Nezuko é transformada em uma demônia. Tanjiro busca uma forma de curar Nezuko e vingar sua família, enfrentando vários demônios e aprendendo técnicas de respiração especiais. A obra se passa no Japão da era Taishō e explora temas como perseverança, amizade, bondade e sacrifício.",
+    "imageUrl": "https://s2-techtudo.glbimg.com/fscjBXjyh15CsR0umO4h0i8O05M=/0x0:1280x720/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/J/8/EbfA0BTv2pV98gASWdfA/f5d0f04e85a224493fe9caa2e59740811630901421-main.png",
+    "facts": {
+      "creatorOfManga": "Koyoharu Gotōge",
+      "producerOfManga": "Yuma Takahash",
+      "studioOfManga": "Shueisha"
+    },
+    "extraInfo": {
+      "seasons": 5,
+      "episodes": 56,
+      "classification": "A14",
+      "assessment": 4.9,
+      "streaming": "Netflix",
+      "song": ["Gurenge(interpretada LiSA)"]
+    }
+  },
+  {
+    "id": "death-note",
+    "name": "Death Note",
+    "shortDescription": "Suspense, Sobrenatural, Psicológico",
+    "description": "Baseado no mangá de Tsugumi Ohba e Takeshi Obata. A história acompanha Light Yagami, um estudante genial que encontra um caderno sobrenatural: o “Death Note”, que pertencia ao shinigami Ryuk, e concede ao usuário a habilidade de matar qualquer pessoa cujo nome seja escrito em suas páginas. Light decide usar o caderno para criar um novo mundo livre de mal, mas entra em conflito com L, um detetive misterioso que tenta capturar e deter Kira, o apelido dado ao assassino em série.",
+    "imageUrl": "https://s2-techtudo.glbimg.com/1Q76caEdN3BriFZ_l61VD5MJkYs=/0x0:1280x720/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/g/0/s4BJiLQGSb9USN3vTvAg/death-note-netflix.jpg",
+    "facts": {
+      "creatorOfManga": "Tsugumi Ohba",
+      "producerOfManga": "Tetsuro Araqui",
+      "studioOfManga": "Madhouse"
+    },
+    "extraInfo": {
+      "seasons": 1,
+      "episodes": 37,
+      "classification": "A16",
+      "assessment": 4.8,
+      "streaming": "Netflix",
+      "song": ["the WORLD(interpretada banda japonesa Nightmare)"]
+    }
+  },
+
+  {
+    "id": "cardcaptor-sakura",
+    "name": "Cardcaptor Sakura",
+    "shortDescription": "Comédia, Aventura",
+    "description": "Baseado no mangá de mesmo nome do grupo Clamp. A história gira em torno de Sakura Kinomoto, uma estudante do quarto ano que descobre poderes mágicos após libertar acidentalmente um conjunto de cartas mágicas chamadas Cartas Clow. Ela deve recuperar as cartas para evitar uma catástrofe no mundo. Cada carta tem poderes diferentes e só pode ser ativada por alguém com habilidades mágicas inerentes. O anime tem 70 episódios e dois filmes. Uma sequência chamada Cardcaptor Sakura: Clear Card começou a ser publicada em 2016.",
+    "imageUrl": "https://assets.gamearena.gg/wp-content/uploads/2023/12/04084244/cardcaptor-sakura.jpg",
+    "facts": {
+      "creatorOfManga": "CLAMP",
+      "producerOfManga": "Eizo Kondo",
+      "studioOfManga": "Madhouse"
+    },
+    "extraInfo": {
+      "seasons": 1,
+      "episodes": 22,
+      "classification": "L",
+      "assessment": 4.8,
+      "streaming": "Crunchyroll",
+      "song": ["Catch You Catch Me (interpretada por Gumi)"]
+    }
+  },
+  {
+    "id": "bleach",
+    "name": "Bleach",
+    "shortDescription": "Ação, Aventura, Fantasia, Sobrenatural",
+    "description": "Bleach é um anime baseado no mangá de mesmo nome de Tite Kubo. A história segue as aventuras de Ichigo Kurosaki, um estudante que pode ver fantasmas e que ganha os poderes de um Shinigami (Ceifador de Almas) de Rukia Kuchiki. Com seus novos poderes, ele tem que defender os humanos dos espíritos malignos chamados Hollows e guiar as almas dos mortos para a Sociedade das Almas. O anime adapta o mangá e também inclui arcos originais com personagens e histórias exclusivas do anime.",
+    "imageUrl": "https://roteironerd.com/wp-content/uploads/2021/08/bleach.jpg",
+    "facts": {
+      "creatorOfManga": "Tite Kubo",
+      "producerOfManga": "Aniplex, Genco, DAX Production e mais",
+      "studioOfManga": "Pierrot"
+    },
+    "extraInfo": {
+      "seasons": 16,
+      "episodes": 366,
+      "classification": "A12",
+      "assessment": 4.7,
+      "streaming": "Star+",
+      "song": ["O anime tem várias músicas, cantadas por diferentes artistas como LiSA, Eir Aoi, Luna Haruna, Shirō Sagisu, entre outros"]
+    }
+  },
+];
