13f9039c7d20618bf7a6593f707b750affa1641b
flaviane-lima <rflaviane41@gmail.com>
2024-03-04 10:01:58 -0300

    troquei a página de visualização para chave api

--- src/views/About.js
+++
@@ -1,7 +0,0 @@
-export function About(props) {
-  const viewEl = document.createElement('div');
-  viewEl.textContent = 'This is the About page.';
-  return viewEl;
-}
-
-// Definir funciones/componentes similares para otras rutas
