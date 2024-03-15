7ff15bafd9ffc9ecc810caf03e664efcb4643b5e
flaviane-lima <rflaviane41@gmail.com>
2024-03-15 19:26:28 -0300

    uma troca de termo para uma string

--- src/lib/openAIApi.js
+++ src/lib/openAIApi.js
@@ -4,38 +4,38 @@
 import { getApiKey } from '../lib/apiKey.js';
 
 
-export const communicateWithOpenAI =  async  (messages) => {
-   //É aqui que você deve implementar a solicitação com fetch ou axios
-   const url = 'https://api.openai.com/v1/chat/completions';
-   const api = "Bearer " + getApiKey();
-   console.log('Iniciando solicitação à API...');
-const req = {
-   method: 'POST',
-   headers: {
-   'Content-Type': 'application/json',
-   'Authorization': api
-   },
-   body: JSON.stringify({
+export const communicateWithOpenAI = async (messages) => {
+  //É aqui que você deve implementar a solicitação com fetch ou axios
+  const url = 'https://api.openai.com/v1/chat/completions';
+  const api = "Bearer " + getApiKey();
+ 
+  const req = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': api
+    },
+    body: JSON.stringify({
       model: "gpt-3.5-turbo",
       temperature: 0.7,
       messages
-   }),
- }
- console.log(req);
- try{
-   const options = await fetch(url,req);
-   console.log('Solicitação concluída, verificando status...');
-if(!options.ok){
-   throw new Error('Falha na conexão com a API');
-}
-console.log('Status da solicitação OK, aguardando resposta JSON...');
-const data = await options.json();
-console.log('Resposta JSON recebida com sucesso:', data);
-return data;
-}catch(error){
-   console.error('Ocorreu um erro:', error.messages);
-   throw error;
-}
+    }),
+  }
+  
+  try {
+    const options = await fetch(url, req);
+   
+    if (!options.ok) {
+      throw new Error('Falha na conexão com a API');
+    }
+   
+    const data = await options.json();
+  
+    return data;
+  } catch (error) {
+    
+    throw new Error('Falha na conexão com a Api');
+  }
 };
 
 
