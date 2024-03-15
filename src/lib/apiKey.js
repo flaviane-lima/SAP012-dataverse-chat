61fd2bb457f5fa86180d058c09fc62a3cd83b851
flaviane-lima <rflaviane41@gmail.com>
2024-03-15 19:25:10 -0300

    troquei a parte do caht que está nos parênteses que estav error para uma com string

--- src/lib/apiKey.js
+++ src/lib/apiKey.js
@@ -2,18 +2,18 @@
 
 export const getApiKey = () => {
   // Implemente o código para obter a API KEY do armazenamento local, obtém a API KEY.
-    const apiKey = localStorage.getItem('chave-api')
-   
-    // if(!apiKey){
-    //   // vai dar erro se a chave não for encontrarada
-    //   throw new Error('chave apiKey não encontrada no local armazenado');
-    // }
-    return apiKey;
-  
+  const apiKey = localStorage.getItem('chave-api')
+
+  // if(!apiKey){
+  //   // vai dar erro se a chave não for encontrarada
+  //   throw new Error('chave apiKey não encontrada no local armazenado');
+  // }
+  return apiKey;
+
 };
 
 export const setApiKey = (key) => {
-  
+
   // Implemente o código para salvar a API KEY no armazenamento local
   localStorage.setItem('chave-api', key); //ela já está salva pois localStorage.setItem já faz isso
 };
