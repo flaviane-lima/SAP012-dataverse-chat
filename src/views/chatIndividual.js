ca0fe67f279e7493088651d58e5536b04a66e3c3
flaviane-lima <rflaviane41@gmail.com>
2024-03-15 19:35:33 -0300

    identação do código

--- src/views/chatIndividual.js
+++ src/views/chatIndividual.js
@@ -1,15 +1,15 @@
 //estrutura do chat individual
 import { filterById } from "../lib/dataFunctions.js";
 import data from "../data/dataset.js";
-import {communicateWithOpenAI} from "../lib/openAIApi.js";
+import { communicateWithOpenAI } from "../lib/openAIApi.js";
 
 export default function chatIndividual(props) {
   document.title = "Chat individual";
- 
-    const dataAnime = filterById(data, props["id"]);
-  
+
+  const dataAnime = filterById(data, props["id"]);
+
   const viewEl = document.createElement('div'); // Definir viewEl antes de usá-lo
-  
+
   viewEl.innerHTML = `
   <div class="chat-individual">
     <section>
@@ -39,77 +39,77 @@ export default function chatIndividual(props) {
   const inputButton = viewEl.querySelector('.chat-input');
 
   //para recebeber uma lista de objetos vazios
-   let menssagens = [];
+  let menssagens = [];
 
-   //função de enviar e receber menssagem
-   const envioDeMenssagem = (event) => {
+  //função de enviar e receber menssagem
+  const envioDeMenssagem = (event) => {
     event.preventDefault();
-   const resposta =  inputButton.value;
-   
-   //capturar uma ul
-   const ul = viewEl.querySelector('#chat-menssagem');
-   //criar li dinamicamente
-   const li = document.createElement('li');
-   li.classList.add('mensagem-enviada');
-   //recebendo conteudo do input
-   li.innerHTML = `<span>${resposta}</span>`;
-   ul.appendChild(li);
-   inputButton.value = "" //limpa
+    const resposta = inputButton.value;
+
+    //capturar uma ul
+    const ul = viewEl.querySelector('#chat-menssagem');
+    //criar li dinamicamente
+    const li = document.createElement('li');
+    li.classList.add('mensagem-enviada');
+    //recebendo conteudo do input
+    li.innerHTML = `<span>${resposta}</span>`;
+    ul.appendChild(li);
+    inputButton.value = "" //limpa
 
 
-   //para bloquear o nome digitando
-   chatRespondendo.style.display = "block"; 
+    //para bloquear o nome digitando
+    chatRespondendo.style.display = "block";
 
-   //if para mensagem iniciar com system
-   if(menssagens.length === 0){
-     menssagens = [{
-      "role": "system",
-      "content":`a partir de agora responda a tudo que eu disser nesta conversa como se fosse ${dataAnime.extraInfo.characters.characterName} do anime ${dataAnime.name}. Considere a seguinte personalidade: ${dataAnime.extraInfo.characters.personality}`
-    }];
+    //if para mensagem iniciar com system
+    if (menssagens.length === 0) {
+      menssagens = [{
+        "role": "system",
+        "content": `a partir de agora responda a tudo que eu disser nesta conversa como se fosse ${dataAnime.extraInfo.characters.characterName} do anime ${dataAnime.name}. Considere a seguinte personalidade: ${dataAnime.extraInfo.characters.personality}`
+      }];
 
-   }
-   console.log(typeof menssagens);
-   menssagens.push({
-    "role": "user",
-    "content": resposta
-  });
+    }
+    // console.log(typeof menssagens);
+    menssagens.push({
+      "role": "user",
+      "content": resposta
+    });
 
-  //integrar com openAI
-  communicateWithOpenAI(menssagens).then(assistentAnswer => {
-    // console.log("Resposta da OpenAI:", assistentAnswer); // Adicione este console.log
-  
-    const choice = assistentAnswer?.choices?.[0];
-    const assistantMessage = choice?.message?.content?.trim();
-  
-    if (assistantMessage) {
+    //integrar com openAI
+    communicateWithOpenAI(menssagens).then(assistentAnswer => {
+      // console.log("Resposta da OpenAI:", assistentAnswer); // Adicione este console.log
+
+      const choice = assistentAnswer?.choices?.[0];
+      const assistantMessage = choice?.message?.content?.trim();
+
+      if (assistantMessage) {
         const message = {
-            role: "assistant",
-            content: assistantMessage
+          role: "assistant",
+          content: assistantMessage
         };
 
         menssagens.push(message);
 
         chatRespondendo.style.display = "none"; //aparece digitando
 
-  
+
         // Exibir a mensagem na interface do usuário
         // const chatMensagens = document.getElementById('chat-menssagem');
         const liSegundo = document.createElement('li');
         liSegundo.classList.add('mensagem-resposta');
         liSegundo.innerHTML = `<span>${assistantMessage}</span>`;
         ul.appendChild(liSegundo);
-    } else {
+      } else {
         console.error("Resposta do assistente inválida:", assistentAnswer);
-    }
-  }).catch(error => {
-    console.error("Erro ao comunicar com OpenAI:", error);
-  });
+      }
+    }).catch(error => {
+      console.error("Erro ao comunicar com OpenAI:", error);
+    });
 
 
   }
 
   //evento de click do botão de enviar
-  viewEl.querySelector('.envio').addEventListener("click", envioDeMenssagem );
+  viewEl.querySelector('.envio').addEventListener("click", envioDeMenssagem);
 
 
   return viewEl;
