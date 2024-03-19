3133373b7aaef7d479f19fe9bec3caed7984ace5
flaviane-lima <rflaviane41@gmail.com>
2024-03-19 12:41:51 -0300

    foi feito ultimo teste para verificar a função se está funcionando corretamente

--- test/router.spec.js
+++ test/router.spec.js
@@ -1,6 +1,31 @@
-import { setRootEl, setRoutes, navigateTo } from '../src/router';
+import { setRootEl, setRoutes, navigateTo} from '../src/router';
+const ERRORPATH = '/error'
+let rootEl;
+let ROUTES;
+const routerFunctions = {
+  renderView: (pathname, props = { name: " ", id: "" }) => {
+
+    //limpa o elemento raiz
+    rootEl.textContent = "";
+
+    let path = "";
+
+    //encontra a visualização correta em ROUTES para o nome do caminho
+    if (pathname in ROUTES) {
+      path = pathname;
+    }
+    else {
+      // caso não seja encontrado renderiza a view do erro
+      path = ERRORPATH
+    }
+
+    // renderiza a view correta passando o valor das props
+    //adiciona o elemento view ao elemento raiz do DOM
+    rootEl.appendChild(ROUTES[path](props));
 
 
+  }
+};
 describe('setRootEl', () => {
   it('deve retornar erro se root element não for objeto', () => {
     const fakeRootEl = "testando o elemento";
@@ -42,11 +67,8 @@ describe('setRoutes', () => {
         const div = document.createElement("div");
         div.innerHTML = "error";
         return div;
-      } 
-      // "/chave-api": chaveApi,
-      // '/chat-grupo' : chatGrupo,
+      }
       // "/chat-individual": chatIndividual,
-      // "/error": error,
     };
 
     expect(() => setRoutes(routes)).not.toThrow('As rotas devem definir uma rota /error');
@@ -83,21 +105,89 @@ describe('NavigateTo', () => {
   });
 });
 
+//teste para onURLChange
+describe('onURLChange', () => {
+
+  beforeAll(() => {
+    //criando um novo elemento "div", para redenrizar as visuaslizações durante os testes.
+    rootEl = document.createElement('div');
+    setRootEl(rootEl);
+
+    //está adicionado novos elementos recém-criado ao corpo do documento HTML.
+    document.body.appendChild(rootEl);
+
+    //criando um "spay" que é um tipo de espião 
+    // renderView = jest.fn();
+    // queryStringToObject = jest.fn();
+
+    const routes = {
+      "/": () => {
+        const div = document.createElement("div");
+        div.innerHTML = "Home";
+        return div;
+      },
+      "/error": () => {
+        const div = document.createElement("div");
+        div.innerHTML = "error";
+        return div;
+      },
+      // "/chat-individual": chatIndividual,
+      "/chat-individual": () => {
+        
+        console.log("teste");
+        const div = document.createElement("div");
+        div.innerHTML = "chat-individual";
+        return div;
+      }
+    };
+
+
+    ROUTES = routes;
+  });
+
+  //está para limpar o ambiente do teste e restaura as funções "espionadas" ("spy").
+  afterAll(() => {
+    document.body.removeChild(rootEl);
+    // renderView();
+    // queryStringToObject();
+  });
+
+  it('deve renderizar a visualização individual do chat com os parâmetros de consulta corretos', () => {
+    const spyRenderView = jest.spyOn(routerFunctions, 'renderView');
+    //Simula o clique em um anime com ID 'death-note'
+    const location = {
+      pathname: '/chat-individual',
+      search: '?id=death-note' //Simula o parâmetro de consulta para 'death-note'
+    };
+
+    //Atualiza o ID em queryStringToObjectSpy para corresponder ao anime clicado
+    const queryStringToObject = (queryString) => {
+
+      const params = new URLSearchParams(queryString);
+      const id = params.get('id') || 'defaultId';
+      return { id };
+    };
+
+    const props = queryStringToObject(location.search);
+    
+    routerFunctions.renderView(location.pathname, props);
+  
+    expect(props).toEqual({ id: 'death-note' });
+    expect(spyRenderView).toHaveBeenCalledWith('/chat-individual', { id: 'death-note' });
+    
+    
+  });
+});
 
 
 
 
-// describe('requerObjeto', () => {
-//   it('deve retornar uma propriedade nome', () => {
-//     const queryString = 'nome = exemplo'
-//     const resultado= queryStringToObject(queryString)
-//     expect(resultado).toHaveProperty('nome');
-//   });
 
-//   test('deve retornar um objeto sem a propriedade nome', () => {
-//     const queryString = 'sobrenome = exemplo'
-//     const resultado = queryStringToObject(queryString)
-//     expect(resultado).not.toHaveProperty('nome');
-//   });
-// });
+
+
+
+
+
+
+
 
