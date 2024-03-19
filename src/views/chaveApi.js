eb4f0b45d9e040ce745288c6ae70df06a946ffa2
flaviane-lima <rflaviane41@gmail.com>
2024-03-19 20:49:58 -0300

    tirei as linhas de códigos que estava comentada e não era precisa no código

--- src/views/chaveApi.js
+++ src/views/chaveApi.js
@@ -5,34 +5,14 @@ export default function chaveApi() {
   const viewEl = document.createElement('div');
   viewEl.innerHTML = `<main>
   <h3>Administrador de chave de API</h3>
-  <P>A partir daqui você pode gerenciar a chave de API a ser usada</P>
+  <p class="texto">A partir daqui você pode gerenciar a chave de API a ser usada</p>
   <label for="chave"></label>
+  <div class="flex-container">
   <input type="text" id="chave" name="chave" maxlength="53" required>
   <button id="salvarOuDeletarChave" type="button">Salvar</button>
+  </div>
 </main>`;
 
-
-  // const button = viewEl.querySelector('#salvarOuDeletarChave');
-  // const chaveSalva = getApiKey();
-
-  // if (chaveSalva) {
-  //   button.textContent = 'Deletar';
-  // }
-
-  // button.addEventListener('click', function() {
-  //   const chave = viewEl.querySelector('#chave').value;
-
-  //   if (getApiKey()) {
-  //     removeApiKey();
-  //     button.textContent = 'Salvar'; // Atualiza o texto do botão após remover a chave
-  //   } else {
-  //     setApiKey(chave);
-  //     button.textContent = 'Deletar';
-  //   }
-  // });
-
-  // return viewEl;
-  // }
   const button = viewEl.querySelector('#salvarOuDeletarChave');
   const chaveSalva = getApiKey();
   const input = viewEl.querySelector('#chave');
