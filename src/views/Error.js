b02105652de3896acc0732bb99f29b809eb96238
flaviane-lima <rflaviane41@gmail.com>
2024-03-25 09:51:36 -0300

    fiz linhas de códigos para exibir a mensagem de error na página

--- src/views/Error.js
+++ src/views/Error.js
@@ -1,7 +1,10 @@
+// views/error.js
 export default function error() {
-  const viewEl = document.createElement('div');
-  viewEl.textContent = `error!`;
+  const viewEl = document.createElement('p');
+  viewEl.textContent = 'Error! Volte para a página inicial';
+  viewEl.classList.add('error-message');
   return viewEl;
 }
+
 
 
