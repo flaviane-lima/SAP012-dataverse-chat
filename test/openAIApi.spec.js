c7e6882dcb2c85f10c4f62009f85c183ff765f67
flaviane-lima <rflaviane41@gmail.com>
2024-03-26 10:09:58 -0300

    coloquei um teste para cobrir a função de error

--- test/openAIApi.spec.js
+++ test/openAIApi.spec.js
@@ -1,4 +1,4 @@
-import { communicateWithOpenAI } from "../src/lib/openAiApi";
+import { communicateWithOpenAI, handleAPIError } from "../src/lib/openAIApi"
 /* eslint-disable */
 // global.fetch = jest.fn().mockResolvedValue({
 //     status: 200, json: jest.fn().mockResolvedValue({})
@@ -32,5 +32,18 @@ describe('Testes de integração com OpenAi', () => {
     const mockMessages = []; // Mensagens vazias
     const mockError = new Error('Falha na conexão com a Api');
     await expect(communicateWithOpenAI(mockMessages)).rejects.toThrow(mockError);
+  });
+});
+
+describe('handleAPIError', () => {
+  test('retorna mensagem correta para erro 429', () => {
+
+    const response = { status: 429, ok:false} ;
+    expect(handleAPIError(response)).toBe('Você atingiu a cota de tokens por minuto. Por favor, aguarde um momento antes de tentar novamente.');
+  });
+
+  test('retorna mensagem correta para erro 500', () => {
+    const response = { status: 500, ok:false };
+    expect(handleAPIError(response)).toBe('Ocorreu um erro ao interagir com a API. Por favor, tente novamente mais tarde.');
   });
 });
